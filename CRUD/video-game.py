53088098ccfe05d3c2d3eb02b749336d682b9aea
Kevin jimenez <kivito2009@hotmail.com>
2019-05-16 21:56:58 -0500

    listar y crear

--- CRUD/video-game.py
+++ CRUD/video-game.py
@@ -1,19 +1,7 @@
 import json
 
 def calculadora(numero_uno,numero_dos,operacion="sumar"):
-    def sumar():
-        return numero_uno + numero_dos
 
-    def restar():
-        return numero_uno - numero_dos
-
-    def dividir():
-        return numero_uno / numero_dos
-
-    def multiplicar():
-        return numero_uno * numero_dos
-
-    
     def crear_archivo():
         contenido = {
             "consolas":[]
@@ -33,19 +21,57 @@ def calculadora(numero_uno,numero_dos,operacion="sumar"):
         else:                
             try:
                 archivo_leido = open("./prueba.json","r") # r por defecto                
-                contenido = json.loads(archivo_leido.read())                
-                #contenido['consolas'].append(json.dumps({"nombre":"ps4","precio":400.99}))                
-                return contenido['consolas']                
+                contenido = json.loads(archivo_leido.read())                                
+                archivo_leido.close()
+                return contenido                
             except Exception as error:                
-                return "Hubo un error"+error
+                return False
+
+    def escribir_archivo(registro):    
+        print("mi regitro",registro)    
+        contenido = leer_archivo()  
+        if contenido:
+            try:                                               
+                contenido['consolas'].append(registro)                
+                archivo_escribir = open("./prueba.json","w") # r por defecto                
+                nuevo_registro = json.dumps(contenido)                
+                archivo_escribir.write(nuevo_registro)
+                archivo_escribir.close()
+                return 'registro creado'                
+            except Exception as error:                
+                return "Hubo un error"
+        else:
+            return 'error al leer'
+            
+
+    def crear_registro():
+        print("Nombre de la empresa")
+        empresa = input()        
+        print("Nombre de la consola")
+        consola = input()        
+        print("Precio de la consola")
+        precio = input()        
+        registro = {
+            "empresa":empresa,
+            "consola":consola,
+            "precio":precio
+        }
+        escribir_archivo(registro)
+        return "registro creado"
+
+
+    def listar_registros():
+        contenido = leer_archivo()
+        if contenido:
+            for consola in contenido['consolas']:
+                print('Empresa: ',consola["empresa"] ,'-','Consola:',consola["consola"],'-','Precio:',consola["precio"])
+        else:
+            print('error')        
         
 
     def operacionesCalculadora():
-        return{
-            "sumar":sumar(),
-            "restar":restar(),
-            "multiplicar":multiplicar(),
-            "crear_archivo":leer_archivo()
+        return{            
+            "crear_archivo":listar_registros()
         }[operacion]
 
     return operacionesCalculadora()
